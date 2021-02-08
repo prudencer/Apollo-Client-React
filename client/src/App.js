3e5a733c5bbebe9dfbeadbd2690de62c5e2df206
Mukondleteri <Mukondleteri@gmail.com>
2021-02-08 22:09:56 +0200

    Basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div class="container" style={{width: 600, margin: "auto"}}>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style={{borderBottomLeftRadius: 10, borderBottomRightRadius: 10}}>
+            <a class="navbar-brand" href="#">
+              <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
+              Chuck
+            </a>
+          </nav>
+
+          <div style={{padding: 20}}>
+            <div class="input-group">
+              <select class="custom-select" id="inputGroupSelect04">
+                <option selected>Choose category...</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">Get Joke</button>
+              </div>
+            </div>
+          </div>
+
+          <div style={{padding: 20}}>
+            <div class="card" style={{borderRadius: 10, padding: 10, margin: "auto"}}>
+              <img class="card-img-top" style={{margin: "auto",width: 150}} src="https://assets.chucknorris.host/img/avatar/chuck-norris.png" alt="Card image cap"/>
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            </div>
+
+        </div>
     </div>
   );
 }
---
+++ client/src/components/navBar.js
@@ -0,0 +1,9 @@
+const navBar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        </nav>
+    )
+}
+
+export default navBar;
